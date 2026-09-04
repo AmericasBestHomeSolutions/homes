1b164a6979b41ad23b9017c2d5a293b9dc5403a2
Joseph Hernandez <josephhernandez@Josephs-Mini.lan>
2026-09-04 08:01:41 -0400

    Refresh listings from master

--- homes.js
+++ homes.js
@@ -13,7 +13,7 @@ const CONTACT = {
   makeWebhook: "https://hook.us2.make.com/h9jsckkipxxm4ngezfzldtflx4y2efo9",
 };
 
-/* --- the homes (146 live, rebuilt from master published 2026-09-02) --- */
+/* --- the homes (146 live, rebuilt from master published 2026-09-03) --- */
 const HOMES = [
   {
     id: "jackson-ms-434-mason",

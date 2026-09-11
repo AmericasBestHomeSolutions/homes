166d4d980047c3b2af62ce28c05c6fb0e40f6f7f
Joseph Hernandez <josephhernandez@Josephs-Mac-mini.local>
2026-09-11 08:03:49 -0400

    Refresh listings from master

--- homes.js
+++ homes.js
@@ -13,7 +13,7 @@ const CONTACT = {
   makeWebhook: "https://hook.us2.make.com/h9jsckkipxxm4ngezfzldtflx4y2efo9",
 };
 
-/* --- the homes (151 live, rebuilt from master published 2026-09-09) --- */
+/* --- the homes (151 live, rebuilt from master published 2026-09-11) --- */
 const HOMES = [
   {
     id: "woodriver-il-46-eckhard",
@@ -3030,7 +3030,7 @@ const HOMES = [
     termText:       "30-year owner financing (contract for deed)",
 
     heroPhoto: "",
-    photos:    [],
+    photos:    ["images/decatur-il-1749-college-1.jpg", "images/decatur-il-1749-college-2.jpg", "images/decatur-il-1749-college-3.jpg", "images/decatur-il-1749-college-4.jpg", "images/decatur-il-1749-college-5.jpg", "images/decatur-il-1749-college-6.jpg", "images/decatur-il-1749-college-7.jpg", "images/decatur-il-1749-college-8.jpg", "images/decatur-il-1749-college-9.jpg", "images/decatur-il-1749-college-10.jpg", "images/decatur-il-1749-college-11.jpg"],
   },
   {
     id: "decatur-il-231-grand",
@@ -3050,7 +3050,7 @@ const HOMES = [
     termText:       "30-year owner financing (contract for deed)",
 
     heroPhoto: "",
-    photos:    [],
+    photos:    ["images/decatur-il-231-grand-1.jpg", "images/decatur-il-231-grand-2.jpg", "images/decatur-il-231-grand-3.jpg", "images/decatur-il-231-grand-4.jpg", "images/decatur-il-231-grand-5.jpg", "images/decatur-il-231-grand-6.jpg", "images/decatur-il-231-grand-7.jpg", "images/decatur-il-231-grand-8.jpg", "images/decatur-il-231-grand-9.jpg", "images/decatur-il-231-grand-10.jpg", "images/decatur-il-231-grand-11.jpg", "images/decatur-il-231-grand-12.jpg", "images/decatur-il-231-grand-13.jpg", "images/decatur-il-231-grand-14.jpg", "images/decatur-il-231-grand-15.jpg", "images/decatur-il-231-grand-16.jpg", "images/decatur-il-231-grand-17.jpg", "images/decatur-il-231-grand-18.jpg", "images/decatur-il-231-grand-19.jpg", "images/decatur-il-231-grand-20.jpg", "images/decatur-il-231-grand-21.jpg", "images/decatur-il-231-grand-22.jpg", "images/decatur-il-231-grand-23.jpg", "images/decatur-il-231-grand-24.jpg"],
   },
   {
     id: "peoria-il-2210-wiswall",
@@ -3070,7 +3070,7 @@ const HOMES = [
     termText:       "30-year owner financing (contract for deed)",
 
     heroPhoto: "",
-    photos:    [],
+    photos:    ["images/peoria-il-2210-wiswall-1.jpg", "images/peoria-il-2210-wiswall-2.jpg", "images/peoria-il-2210-wiswall-3.jpg", "images/peoria-il-2210-wiswall-4.jpg", "images/peoria-il-2210-wiswall-5.jpg", "images/peoria-il-2210-wiswall-6.jpg", "images/peoria-il-2210-wiswall-7.jpg", "images/peoria-il-2210-wiswall-8.jpg", "images/peoria-il-2210-wiswall-9.jpg", "images/peoria-il-2210-wiswall-10.jpg", "images/peoria-il-2210-wiswall-11.jpg", "images/peoria-il-2210-wiswall-12.jpg", "images/peoria-il-2210-wiswall-13.jpg", "images/peoria-il-2210-wiswall-14.jpg", "images/peoria-il-2210-wiswall-15.jpg", "images/peoria-il-2210-wiswall-16.jpg", "images/peoria-il-2210-wiswall-17.jpg", "images/peoria-il-2210-wiswall-18.jpg", "images/peoria-il-2210-wiswall-19.jpg", "images/peoria-il-2210-wiswall-20.jpg", "images/peoria-il-2210-wiswall-21.jpg", "images/peoria-il-2210-wiswall-22.jpg", "images/peoria-il-2210-wiswall-23.jpg"],
   },
   {
     id: "david-il-209-central",
